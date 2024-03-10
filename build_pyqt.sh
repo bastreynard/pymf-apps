73d8e59d88820f20f010979d0b891cc68fa56bab
Bastien Reynard <bastien@rnrd.ch>
2024-03-10 19:10:32 +0100

    Clean build script

--- build_pyqt.sh
+++ build_pyqt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rm -rf dist/ build/
-source venv.sh
-pip install -e pyqt/QtWaitingSpinner
+source venv.sh -d
 pyinstaller run_pyqt.py --name MovieFinder --icon=resources/imdb.ico --windowed --clean --add-data 'resources/imdb.png;.' --onefile
